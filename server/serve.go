7c9971231b6b6e69d8a9a08b2f254631c252e3e7
mogeta <muzigen@gmail.com>
2016-08-17 19:14:52 +0900

    create get message api

--- server/serve.go
+++ server/serve.go
@@ -22,9 +22,35 @@ type Output struct {
 	Out string
 }
 
+type Format struct {
+	Ok           int
+	ErrorMessage string
+}
+
+type SignalData struct {
+	Format
+	Data string
+}
+
 func jsonHandleFunc(rw http.ResponseWriter, req *http.Request) {
 
 	output := Output{irkit.GetIPAddress()}
+	outjson, err := json.Marshal(output)
+	if err != nil {
+		fmt.Println(err)
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(rw, string(outjson))
+}
+
+func message(rw http.ResponseWriter, req *http.Request) {
+	signal, err := irkit.GetMessages()
+	result := Format{0, ""}
+	if err != nil {
+		result.Ok = 1
+		result.ErrorMessage = "irkitError"
+	}
+	output := SignalData{result, signal}
 	outjson, err := json.Marshal(output)
 	if err != nil {
 		fmt.Println(err)
@@ -38,5 +64,6 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/api", jsonHandleFunc)
+	http.HandleFunc("/message", message)
 	http.ListenAndServe(":8080", nil)
 }
